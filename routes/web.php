16f9218447b08366d33987b4ff090f7d37e434c4
Jerry <1091231787@qq.com>
2019-01-07 15:34:38 +0800

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -50,5 +50,7 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
-    Route::get('user/addresses', 'UserAddressesController@index')->name('user.addresses.index');
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
