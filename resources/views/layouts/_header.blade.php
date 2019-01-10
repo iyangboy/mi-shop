1163f3c0613c8c74f29bb5c8f6f5f8b8cd4e0465
Jerry <1091231787@qq.com>
2019-01-10 10:46:44 +0800

    购物车页面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,8 @@
         <!-- Branding Image -->
         <a class="navbar-brand " href="{{ url('/') }}">Mi Shop
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -12,33 +13,40 @@
             </ul>
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav navbar-right">
-            <!-- 登录注册链接开始 -->
-            @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">登录</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">注册</a>
-                </li>
-            @else
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
-                        {{ Auth::user()->name }}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
-                        <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
-                        <a href="{{ route('products.favorites') }}" class="dropdown-item">我的收藏</a>
-                        <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" id="logout" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </div>
-                </li>
-            @endguest
+                <!-- 登录注册链接开始 -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">登录</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">注册</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link mt-1" href="{{ route('cart.index') }}"><i
+                                class="fa fa-shopping-cart"></i></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px"
+                                 height="30px">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
+                            <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+                            <a href="{{ route('products.favorites') }}" class="dropdown-item">我的收藏</a>
+                            <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" id="logout" href="#"
+                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </li>
+                @endguest
             <!-- 登录注册链接结束 -->
             </ul>
         </div>
