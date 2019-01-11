3c91c7da84c64dbe26473f03fc48c9f9c078583c
Jerry <1091231787@qq.com>
2019-01-11 15:26:29 +0800

    管理后台 - 优惠券列表

---
+++ app/Models/CouponCode.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class CouponCode extends Model
+{
+    // 用常量的方式定义支持的优惠券类型
+    const TYPE_FIXED = 'fixed';
+    const TYPE_PERCENT = 'percent';
+
+    public static $typeMap = [
+        self::TYPE_FIXED   => '固定金额',
+        self::TYPE_PERCENT => '比例',
+    ];
+
+    protected $appends = ['description'];
+
+    protected $fillable = [
+        'name', // 优惠券的标题
+        'code', // 优惠码，用户下单时输入
+        'type', // 优惠券类型，支持固定金额和百分比折扣
+        'value', // 折扣值，根据不同类型含义不同
+        'total', // 全站可兑换的数量
+        'used', // 当前已兑换的数量
+        'min_amount', // 使用该优惠券的最低订单金额
+        'not_before', // 在这个时间之前不可用
+        'not_after', // 在这个时间之后不可用
+        'enabled', // 优惠券是否生效
+    ];
+    protected $casts = [
+        'enabled' => 'boolean',
+    ];
+    // 指明这两个字段是日期类型
+    protected $dates = ['not_before', 'not_after'];
+
+    // 生成优惠券编码
+    public static function findAvailableCode($length = 16)
+    {
+        do {
+            // 生成一个指定长度的随机字符串，并转成大写
+            $code = strtoupper(Str::random($length));
+            // 如果生成的码已存在就继续循环
+        } while (self::query()->where('code', $code)->exists());
+
+        return $code;
+    }
+
+    // 优惠券描述
+    public function getDescriptionAttribute()
+    {
+        $str = '';
+
+        if ($this->min_amount > 0) {
+            $str = '满 ' . str_replace('.00', '', $this->min_amount);
+        }
+        if ($this->type === self::TYPE_PERCENT) {
+            return $str . ' 优惠 ' . str_replace('.00', '', $this->value) . '%';
+        }
+
+        return $str . ' 减 ' . str_replace('.00', '', $this->value);
+    }
+}
