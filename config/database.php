25e271cdada4dd301e2a6471ecdde9b5438932c3
Jerry <1091231787@qq.com>
2019-01-14 19:18:31 +0800

    引入 Elasticsearch

--- config/database.php
+++ config/database.php
@@ -2,6 +2,11 @@
 
 return [
 
+    'elasticsearch' => [
+        // Elasticsearch 支持多台服务器负载均衡，因此这里是一个数组
+        'hosts' => explode(',', env('ES_HOSTS')),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -33,56 +38,56 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
-
+    'sqlite' => [
+        'driver'                  => 'sqlite',
+        'database'                => env('DB_DATABASE', database_path('database.sqlite')),
+        'prefix'                  => '',
+        'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
     ],
+
+    'mysql' => [
+        'driver'         => 'mysql',
+        'host'           => env('DB_HOST', '127.0.0.1'),
+        'port'           => env('DB_PORT', '3306'),
+        'database'       => env('DB_DATABASE', 'forge'),
+        'username'       => env('DB_USERNAME', 'forge'),
+        'password'       => env('DB_PASSWORD', ''),
+        'unix_socket'    => env('DB_SOCKET', ''),
+        'charset'        => 'utf8mb4',
+        'collation'      => 'utf8mb4_unicode_ci',
+        'prefix'         => '',
+        'prefix_indexes' => true,
+        'strict'         => true,
+        'engine'         => null,
+    ],
+
+    'pgsql' => [
+        'driver'         => 'pgsql',
+        'host'           => env('DB_HOST', '127.0.0.1'),
+        'port'           => env('DB_PORT', '5432'),
+        'database'       => env('DB_DATABASE', 'forge'),
+        'username'       => env('DB_USERNAME', 'forge'),
+        'password'       => env('DB_PASSWORD', ''),
+        'charset'        => 'utf8',
+        'prefix'         => '',
+        'prefix_indexes' => true,
+        'schema'         => 'public',
+        'sslmode'        => 'prefer',
+    ],
+
+    'sqlsrv' => [
+        'driver'         => 'sqlsrv',
+        'host'           => env('DB_HOST', 'localhost'),
+        'port'           => env('DB_PORT', '1433'),
+        'database'       => env('DB_DATABASE', 'forge'),
+        'username'       => env('DB_USERNAME', 'forge'),
+        'password'       => env('DB_PASSWORD', ''),
+        'charset'        => 'utf8',
+        'prefix'         => '',
+        'prefix_indexes' => true,
+    ],
+
+],
 
     /*
     |--------------------------------------------------------------------------
@@ -110,22 +115,22 @@ return [
 
     'redis' => [
 
-        'client' => 'predis',
+    'client' => 'predis',
 
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
-        ],
-
-        'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
-        ],
-
+    'default' => [
+        'host'     => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port'     => env('REDIS_PORT', 6379),
+        'database' => env('REDIS_DB', 0),
     ],
+
+    'cache' => [
+        'host'     => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port'     => env('REDIS_PORT', 6379),
+        'database' => env('REDIS_CACHE_DB', 1),
+    ],
+
+],
 
 ];
