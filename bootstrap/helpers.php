285bc22eb9dfe6cf02de1f3a828b15cf76376a8e
Jerry <1091231787@qq.com>
2019-01-14 15:36:56 +0800

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -20,3 +20,10 @@ function ngrok_url($routeName, $parameters = [])
 
     return route($routeName, $parameters);
 }
+
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    // 引入 moontoast/math 扩展
+    return new \Moontoast\Math\BigNumber($number, $scale);
+}
