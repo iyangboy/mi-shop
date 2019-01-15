eaa79ac45fcd87ad64d467ace1be216a838bfd42
Jerry <1091231787@qq.com>
2019-01-15 15:42:14 +0800

    推荐相似商品

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -109,4 +109,9 @@ class Product extends Model
                 return $properties->pluck('value')->all();
             });
     }
+
+    public function scopeByIds($query, $ids)
+    {
+        return $query->whereIn('id', $ids)->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $ids)));
+    }
 }
