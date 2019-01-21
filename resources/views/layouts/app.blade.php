dc2bb5dec2e23998ae8e04190b770756f0186a88
Jerry <1091231787@qq.com>
2019-01-21 17:34:08 +0800

    说明文件

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Mi Shop') - 小米电商 面试使用</title>
+    <title>@yield('title', 'Mi Shop') - 电商 面试使用</title>
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
