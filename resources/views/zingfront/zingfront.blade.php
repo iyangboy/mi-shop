9c9b5743517fc284118fcd430577ce68d5edbd37
Jerry <1091231787@qq.com>
2019-02-12 11:45:07 +0800

    题三，个人简历

--- resources/views/zingfront/zingfront.blade.php
+++ resources/views/zingfront/zingfront.blade.php
@@ -3,5 +3,5 @@
     <span style="display:inline-block;width: 15px;"></span>
     <a href="{{ route('num-sum.index') }}" class="float">题二</a>
     <span style="display:inline-block;width: 15px;"></span>
-    <a href="{{ route('zingfront.jerry') }}" class="float">题三</a>
+    <a href="{{ route('zingfront.jerry') }}" class="float">题三(个人简历)</a>
 </div>
